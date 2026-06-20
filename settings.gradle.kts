77173b82caec39a0858e608493fabdaef550b762
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-06-20 15:10:14 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,15 +5,15 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
     }
-}
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
 }
 
 rootProject.name = "nullaway-optional-issue"
